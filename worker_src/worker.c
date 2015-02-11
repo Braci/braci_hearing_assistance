36dc319bbdc2aadba98ecb4fa583e2dc2b2ab2e2
Semyon Maryasin <simeon@maryasin.name>
2015-02-11 21:43:19 +0300

    worker: log accel gatherings

--- worker_src/worker.c
+++ worker_src/worker.c
@@ -1,10 +1,17 @@
 #include <pebble_worker.h>
 
+// Available: 10, 25, 50 or 100 hz
+#define SAMPLING_RATE ACCEL_SAMPLING_10HZ
+#define NUM_SAMPLES 10
+
 void accel_handler(AccelData *data, uint32_t num_samples) {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "%d %ld %d %d %d", data->did_vibrate, (long)data->timestamp,
+			data->x, data->y, data->z);
 }
 
 static void init() {
-	accel_data_service_subscribe(10, accel_handler);
+	accel_data_service_subscribe(NUM_SAMPLES, accel_handler);
+	accel_service_set_sampling_rate(SAMPLING_RATE);
 }
 
 static void deinit() {
