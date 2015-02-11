b927857af5e910b7adb72789dc3c4ac263db4ab7
Semyon Maryasin <simeon@maryasin.name>
2015-02-11 21:36:49 +0300

    Basic worker code

--- src/worker.c
+++ src/worker.c
@@ -1,9 +1,14 @@
 #include <pebble_worker.h>
 
+void accel_handler(AccelData *data, uint32_t num_samples) {
+}
+
 static void init() {
+	accel_data_service_subscribe(10, accel_handler);
 }
 
 static void deinit() {
+	accel_data_service_unsubscribe();
 }
 
 int main(void) {
