c9cbc4e09eb4171c23fc19fd3e4eb889ff7d4725
Semyon Maryasin <simeon@maryasin.name>
2015-02-11 23:24:16 +0300

    countdown: implemented counting and action

--- src/countdown.c
+++ src/countdown.c
@@ -2,19 +2,41 @@
 
 static Window *wnd;
 static TextLayer *tl;
+static int count;
 static char buf[4];
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+	count--;
+	snprintf(buf, sizeof(buf), "%02d", count);
+	text_layer_set_text(tl, buf);
+
+	if(count == 0) {
+		tick_timer_service_unsubscribe();
+		vibes_double_pulse();
+		window_stack_remove(wnd, true);
+	}
+}
+
 static void window_load(Window *wnd) {
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "wndload");
 	Layer *root = window_get_root_layer(wnd);
+
+	count = COUNTDOWN_SECONDS;
+	snprintf(buf, sizeof(buf), "%02d", count);
 
 	tl = text_layer_create(GRect(0,0,144,130));
 	text_layer_set_background_color(tl, GColorBlack);
 	text_layer_set_text_color(tl, GColorWhite);
 	text_layer_set_font(tl, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
-	text_layer_set_text(tl, "10");
+	text_layer_set_text_alignment(tl, GTextAlignmentCenter);
+	text_layer_set_text(tl, buf);
 	layer_add_child(root, text_layer_get_layer(tl));
+
+	tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 static void window_unload(Window *wnd) {
+	tick_timer_service_unsubscribe();
+
 	text_layer_destroy(tl);
 }
 
