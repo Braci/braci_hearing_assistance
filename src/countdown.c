815249987e4b7a92d9c2e390f98d1ecd9548aa3e
Semyon Maryasin <simeon@maryasin.name>
2015-02-11 23:29:23 +0300

    countdown: added separate layers

--- src/countdown.c
+++ src/countdown.c
@@ -1,14 +1,14 @@
 #include "countdown.h"
 
 static Window *wnd;
-static TextLayer *tl;
+static TextLayer *tl_head, *tl_count, *tl_bottom;
 static int count;
 static char buf[4];
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 	count--;
-	snprintf(buf, sizeof(buf), "%02d", count);
-	text_layer_set_text(tl, buf);
+	snprintf(buf, sizeof(buf), "%d", count);
+	text_layer_set_text(tl_count, buf);
 
 	if(count == 0) {
 		tick_timer_service_unsubscribe();
@@ -24,20 +24,38 @@ static void window_load(Window *wnd) {
 	count = COUNTDOWN_SECONDS;
 	snprintf(buf, sizeof(buf), "%02d", count);
 
-	tl = text_layer_create(GRect(0,0,144,130));
-	text_layer_set_background_color(tl, GColorBlack);
-	text_layer_set_text_color(tl, GColorWhite);
-	text_layer_set_font(tl, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
-	text_layer_set_text_alignment(tl, GTextAlignmentCenter);
-	text_layer_set_text(tl, buf);
-	layer_add_child(root, text_layer_get_layer(tl));
+	tl_head = text_layer_create(GRect(0,0,144,70));
+	text_layer_set_background_color(tl_head, GColorBlack);
+	text_layer_set_text_color(tl_head, GColorWhite);
+	text_layer_set_font(tl_head, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+	text_layer_set_text_alignment(tl_head, GTextAlignmentCenter);
+	text_layer_set_text(tl_head, "Will send SOS in");
+	layer_add_child(root, text_layer_get_layer(tl_head));
+
+	tl_count = text_layer_create(GRect(0,70,144,70));
+	text_layer_set_background_color(tl_count, GColorBlack);
+	text_layer_set_text_color(tl_count, GColorWhite);
+	text_layer_set_font(tl_count, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+	text_layer_set_text_alignment(tl_count, GTextAlignmentCenter);
+	text_layer_set_text(tl_count, buf);
+	layer_add_child(root, text_layer_get_layer(tl_count));
+
+	tl_bottom = text_layer_create(GRect(0,140,144,20));
+	text_layer_set_background_color(tl_bottom, GColorBlack);
+	text_layer_set_text_color(tl_bottom, GColorWhite);
+	text_layer_set_font(tl_bottom, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+	text_layer_set_text_alignment(tl_bottom, GTextAlignmentCenter);
+	text_layer_set_text(tl_bottom, "seconds");
+	layer_add_child(root, text_layer_get_layer(tl_bottom));
 
 	tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 }
 static void window_unload(Window *wnd) {
 	tick_timer_service_unsubscribe();
 
-	text_layer_destroy(tl);
+	text_layer_destroy(tl_head);
+	text_layer_destroy(tl_count);
+	text_layer_destroy(tl_bottom);
 }
 
 void countdown_start() {
