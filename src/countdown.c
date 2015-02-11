7dcad92fb9fd17f9bf6a2bc04c5213242c1aa2a9
Semyon Maryasin <simeon@maryasin.name>
2015-02-11 23:10:04 +0300

    countdown: added window

--- src/countdown.c
+++ src/countdown.c
@@ -1,9 +1,24 @@
 #include "countdown.h"
 
+static Window *wnd;
+
+static void window_load(Window *wnd) {
+	//
+}
+static void window_unload(Window *wnd) {
+}
+
 void countdown_start() {
+	window_stack_push(window, true);
 }
 
 void countdown_init() {
+	win = window_create();
+	window_set_window_handlers(wnd, (WindowHandlers) {
+			.load = window_load,
+			.unload = window_unload,
+	});
 }
 void countdown_deinit() {
+	window_destroy(wnd);
 }
