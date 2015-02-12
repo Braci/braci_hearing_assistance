d9d205fb7e32441f768dcbaebf34ceb0aed22540
Semyon Maryasin <simeon@maryasin.name>
2015-02-12 18:11:32 +0300

    paging: added menu callbacks (empty)

--- src/paging.c
+++ src/paging.c
@@ -3,6 +3,17 @@
 static Window *wnd;
 static MenuLayer *menu;
 
+static void menu_draw_header(GContext *ctx, const Layer *layer, uint16_t section_idx, void *cb_ctx) {
+}
+static void menu_draw_row(GContext *ctx, const Layer *layer, MenuIndex *cell_idx, void *cb_ctx) {
+}
+static uint16_t menu_get_num_sections(struct MenuLayer *ml, void *ctx) {
+}
+static uint16_t menu_get_num_rows(struct MenuLayer *ml, uint16_t section_idx, void *cb_ctx) {
+}
+static void menu_select_click(struct MenuLayer *ml, MenuIndex *idx, void *cb_ctx) {
+}
+
 static void window_load(Window *wnd) {
 	Layer *root = window_get_root_layer(wnd);
 	GRect bounds = layer_get_bounds(wnd_layer);
@@ -11,6 +22,7 @@ static void window_load(Window *wnd) {
 	menu_layer_set_callbacks(menu, NULL, (MenuLayerCallbacks) {
 		.draw_header = menu_draw_header,
 		.draw_row = menu_draw_row,
+		.get_num_sections = menu_get_num_sections,
 		.get_num_rows = menu_get_num_rows,
 		.select_click = menu_select_click,
 	});
