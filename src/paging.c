e2e0d0cb864dbb4db437af6ea8660aff171f0b28
Semyon Maryasin <simeon@maryasin.name>
2015-02-12 18:03:24 +0300

    Added menu layer

--- src/paging.c
+++ src/paging.c
@@ -4,8 +4,19 @@ static Window *wnd;
 static MenuLayer *menu;
 
 static void window_load(Window *wnd) {
+	Layer *root = window_get_root_layer(wnd);
+	GRect bounds = layer_get_bounds(wnd_layer);
+
+	menu = menu_layer_create(bounds);
+	menu_layer_set_callbacks(menu, NULL, (MenuLayerCallbacks) {
+		.draw_header = menu_draw_header,
+		.draw_row = menu_draw_row,
+		.get_num_rows = menu_get_num_rows,
+		.select_click = menu_select_click,
+	});
 }
 static void window_unload(Window *wnd) {
+	menu_layer_destroy(menu);
 }
 
 void paging_open() {
