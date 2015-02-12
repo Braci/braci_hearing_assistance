42a90f1f0e09e34a7ad9a8aa1703fea88e2fe916
Semyon Maryasin <simeon@maryasin.name>
2015-02-12 17:57:04 +0300

    paging: added window

--- src/paging.c
+++ src/paging.c
@@ -1,1 +1,24 @@
 #include "paging.h"
+
+static Window *wnd;
+static MenuLayer *menu;
+
+static void window_load(Window *wnd) {
+}
+static void window_unload(Window *wnd) {
+}
+
+void paging_open() {
+	window_stack_push(wnd, true);
+}
+
+void paging_init() {
+	wnd = window_create();
+	window_set_window_handlers(wnd, (WindowHandlers) {
+		.load = window_load,
+		.unload = window_unload
+	});
+}
+void paging_deinit() {
+	window_destroy(wnd);
+}
--- src/paging.h
+++ src/paging.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <pebble.h>
 
 void paging_init();
 void paging_deinit();
+
+void paging_open();
