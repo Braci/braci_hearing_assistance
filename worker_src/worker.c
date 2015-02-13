e63104c224f106a74358d32b083430837ce5dd65
Semyon Maryasin <simeon@maryasin.name>
2015-02-13 21:11:55 +0300

    WIP: integration

--- worker_src/worker.c
+++ worker_src/worker.c
@@ -2,22 +2,65 @@
 
 // This is an accelerometer sampling rate
 // Available: 10, 25, 50 or 100 hz
-#define SAMPLING_RATE ACCEL_SAMPLING_10HZ
-#define NUM_SAMPLES 2
+#define SAMPLING_RATE ACCEL_SAMPLING_25HZ
+// how many samples to approximate before calling our handler
+#define NUM_SAMPLES 1
+// how many results to cache
+#define INTEGRAL_LEN 20
 #define ACCEL_THRESHOLD 2500
 #define mod(x) (x>0?x:-x)
 
-static bool is_fall(int x, int y, int z) {
-	return mod(data->x) > ACCEL_THRESHOLD ||
-			mod(data->y) > ACCEL_THRESHOLD ||
-			mod(data->z) > ACCEL_THRESHOLD;
+static float my_sqrt(float x) {
+	float a, p, e=.001, b;
+	a = x;
+	p = a*a;
+	int nb = 0;
+	while ((p-x >= e) && (nb++ < 50)) {
+		b = (a + (x/a)) / 2;
+		a = b;
+		p = a*a;
+	}
+	return a;
+}
+
+int cpos = 0;
+// x, y, z, norm, v1, v2
+static float coords[INTEGRAL_LEN][4];
+long otime = 0;
+float ox=0, oy=0, oz=0, on=0; // previous values
+float v1=0, v2=0;
+
+static void init_detector() {
+	for(int i=0; i<INTEGRAL_LEN; i++)
+		for(int j=0; j<4; j++)
+			coords[i][j] = 0;
+}
+static bool is_fall(float x, float y, float z, long time) {
+	coords[cpos][0] = x;
+	coords[cpos][1] = y;
+	coords[cpos][2] = z;
+	float norm = my_sqrt(x*x + y*y + z*z);
+	coords[cpos][3] = norm;
+
+	float v1, v2;
+	if(norm < 9.79) { // calculate integral
+		v1 = (norm - 9.81) 
+	} else { // damp
+		v1 = v1 * 0.95;
+	}
+
+	cpos++;
+	if(cpos>=INTEGRAL_LEN)
+		cpos = 0;
+
+	return false;
 }
 
 static void accel_handler(AccelData *data, uint32_t num_samples) {
 	if(data->did_vibrate)
 		return; // don't trust such data!
 
-	if(is_fall(data->x, data->y, data->z)) {
+	if(is_fall(((float)data->x)/100, ((float)data->y)/100, ((float)data->z)/100), data->timestamp) {
 		// Send a message in case the app is already started...
 		AppWorkerMessage msg = {
 			.data0 = (uint16_t)data->x,
@@ -43,6 +86,7 @@ static void message_handler(uint16_t type, AppWorkerMessage *data) {
 
 static void init() {
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Worker: init()");
+	init_detector();
 	app_worker_message_subscribe(message_handler);
 	accel_data_service_subscribe(NUM_SAMPLES, accel_handler);
 	accel_service_set_sampling_rate(SAMPLING_RATE);
